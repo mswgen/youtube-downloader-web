f82f50112c4fb09698659a1a33887e1108b93da5
unknown <enter7377@naver.com>
2021-03-01 22:13:28 +0900

    init

---
+++ app.js
@@ -0,0 +1,95 @@
+// Dependency
+const express = require('express');
+const ytdl = require('ytdl-core');
+const bodyParser = require('body-parser');
+
+// Variables
+const PORT = process.env.PORT || 5000;
+
+// App
+const app = express();
+
+// Middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.json());
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+// Route
+app.get('/', async (req, res) => {
+  res.send('asdf')
+});
+
+app.get('/process', async (req, res) => {
+  let { url } = req.query;
+  let videoArr = [];
+  let audioArr = [];
+
+  if (!url)
+    return res.send(`<script>alert("유효하지 않은 url입니다");location.href='/';</script>`);
+  url = youtube_parser(url);
+  if (url == false)
+    return res.send(`<script>alert("유효하지 않은 url입니다");location.href='/';</script>`);
+
+  ytdl.getInfo(url).then(info => {
+    info.formats.map(createCB('video', videoArr, info));
+    info.formats.map(createCB('audio', audioArr, info));
+
+    res.render('result', {
+      videoArr,
+      audioArr,
+      thumbnail: `https://i.ytimg.com/vi/${url}/hqdefault.jpg`,
+    });
+  });
+});
+
+
+
+app.listen(PORT, () => {
+  console.log(`Listening on Port ${PORT}`);
+})
+
+function youtube_parser(url){
+  var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
+  var match = url.match(regExp);
+  return (match&&match[7].length==11)? match[7] : false;
+}
+
+function createCB(type, arr, info) {
+  if (type == 'video') {
+    const cb = 
+    /**
+     * @param {ytdl.videoFormat} format 
+     */
+    format => {
+      if (arr.length > 5)  return;
+      if (format.hasVideo && !format.hasAudio) {
+        arr.push({
+          title: info.videoDetails.title,
+          url: format.url,
+          fps: format.fps,
+          quality: format.qualityLabel,
+          container: format.container,
+        })
+      }
+    }
+    return cb;
+  } else {
+    const cb = 
+    /**
+     * @param {ytdl.videoFormat} format 
+     */
+    format => {
+      if (arr.length > 5)  return;
+      if (!format.videoCodec && format.hasAudio) {
+        arr.push({
+          title: info.videoDetails.title,
+          url: format.url,
+          quality: format.audioBitrate,
+          container: format.container,
+        });
+      }
+    }
+    return cb;
+  }
+}
